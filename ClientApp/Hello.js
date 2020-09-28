7fa9abf946f10241242f37c13709d2f25bd4bdb9
Daily-Prophet <fuyangzhen@gmail.com>
2020-09-28 19:40:16 +0800

    add ClientApp fold

---
+++ ClientApp/Hello.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Hello extends Component {
+    render () {
+        return (
+            <div>Hello!</div>
+        );
+    }
+}
+
+Hello.propTypes = {};
+Hello.defaultProps = {};
+
+export default Hello;
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
-const APP_DIR = path.resolve(__dirname,'src');
+const APP_DIR = path.resolve(__dirname,'ClientApp');
 const PUBLIC_DIR = path.resolve(__dirname,'public');
 
 const config = {
-    entry: APP_DIR + '/HelloWorld.js',
+    entry: APP_DIR + '/Client.js',
     devServer: {
       contentBase: PUBLIC_DIR,
       port: 9000,
@@ -13,7 +13,27 @@ const config = {
     output: {
         path: PUBLIC_DIR,
         filename: 'bundle.js'
-    }};
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: {
+                    presets: [
+                        'react',
+                        'stage-2',
+                        ['env', {targets: {browsers: ['last 2 versions']}}]
+                    ]
+                }
+            }
+        ]
+    }
+
+
+};
 
 module.exports = config;
 
